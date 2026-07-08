8d86e2d27dd3ce5e95ab802bd63ae4b110599273
Annette <annette@users.noreply.github.com>
2026-07-08 16:40:35 +0800

    Commit .quartz/plugins to repo and remove from .gitignore for CI builds

---
+++ quartz/.quartz/plugins/recent-notes/src/index.ts
@@ -0,0 +1,9 @@
+export { default as RecentNotes } from "./components/RecentNotes";
+export type { RecentNotesOptions } from "./components/RecentNotes";
+
+// Re-export shared types from @quartz-community/types
+export type {
+  QuartzComponent,
+  QuartzComponentProps,
+  StringResource,
+} from "@quartz-community/types";
